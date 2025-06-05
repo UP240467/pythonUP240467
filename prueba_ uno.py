543e7f749fdb2ded3edf6ba55208b0466b0bbf20
UP240467 <up240467@alumnos.upa.edu.mx>
2025-06-05 12:31:28 -0600

    inicio del control de versiones

---
+++ prueba_ uno.py
@@ -0,0 +1,28 @@
+# Parte uno, tengo la version 3.12.5
+
+
+#Parte dos, operaciones
+# Ejercicio uno suma
+print(2 + 2)
+
+# Ejercicio dos multiplicacion
+print(2 * 3) 
+
+# Ejercicio tres resta         
+print(3 - 3)
+
+# Ejercicio cuatro exponencial
+print(2 ** 2)
+
+# Ejercicio cinco modulo
+print(5 % 3)
+
+# Ejercicio seis division 
+print(10 // 5)
+
+
+#Parte tres 
+nombre = input("Escribe tu nombre:")
+print("Hola" , nombre, "Que te vaya bien")
+
+
---
+++ prueba_dos.py
@@ -0,0 +1,27 @@
+# Dia dos de python
+
+primer_nombre, apellido, ciudad, edad, es_soltera = 'Nay', 'Chavez', 'Aguascalientes', 19, True
+print(primer_nombre, apellido, ciudad, edad, es_soltera)
+print('Primer nombre:', primer_nombre)
+print('Apellido: ', apellido)
+print('Ciudad: ', ciudad)
+print('Edad: ', edad)
+print('Es soltera: ', es_soltera)
+
+# Segundo ejercicio
+
+primer_nombre = 'Anayeli'    
+apellido = 'Chavez'       
+ciudad = 'Aguascalientes'                 
+edad = 19                 
+
+print(type('Anayeli'))          
+print(type(primer_nombre))          
+print(type(10))                  
+print(type(3.14))                
+print(type(1 + 1j))              
+print(type(True))               
+print(type([1, 2, 3, 4]))        
+print(type({'name':'Anayeli'})) 
+print(type((1,2)))               
+print(type(zip([1,2],[3,4])))   
